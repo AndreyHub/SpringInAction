7fd1307abf9ef58b0835e8fa5d14284522fc2e35
Hooper <Hooper@192.168.56.1>
2023-12-17 19:02:38 +0300

    2.3 Отображение сообщений о ошибках

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,11 +2,18 @@ package tacos;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Taco {
+	@NotNull
+	@Size(min=5, message ="Имя должно быть не менее 5 символов")
 	private String name;
+	
+	@NotNull
+	@Size(min=1, message="Выберите не менее одного ингредиента")
 	private List<Ingredient> ingredients;
 
 }
--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -3,18 +3,38 @@ package tacos;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class TacoOrder {
 
+	@NotBlank(message = "Имя доставки обязательно должно быть заполнено")
 	private String deliveryName;
+	
+	@NotBlank (message = "Укажите улицу")
 	private String deliveryStreet;
+	
+	@NotBlank (message = "Укажите город")
 	private String deliveryCity;
+	
+	@NotBlank (message = "Укажите регион")
 	private String deliveryState;
+	
+	@NotBlank (message = "Укажите код")
 	private String deliveryZip;
+	
+	@CreditCardNumber(message = "Не корректный номер карты")
 	private String ccNumber;
+	
+	@Pattern(regexp="^(0[1-9][1[0-2])([\\/])([2-9][0-9])$", message="Не корректнаф дата")	
 	private String ccExpiration;
+	
+	@Digits (integer=3, fraction=0, message = "Не корректный код CW")
 	private String ccCW;
 	
 	private List <Taco> tacos = new ArrayList();
